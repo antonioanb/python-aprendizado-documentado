99175b70207ff10fba2eaea76ac2eb4ef89a3bf9
Antonio Barros <antoniobarros.dev@gmail.com>
2025-12-15 23:15:07 -0300

    estudo(tipos_de_dados): explora tipos de dados

---
+++ Fundamentos Sólidos/1_introducao_a_tipos_de_dados.py
@@ -0,0 +1,47 @@
+# um tipo de dado pode ser visto como uma classificação que o python coloca em um valor literal para saber quais operações podem ser feitas com esse dado, tipo operações matematicas se for numero ou concatenação se for texto.
+
+# ele basicamente dita como o computador deve interagir com aqueele pedaço de informação
+
+
+2 # isso é um 2 literal o python o vê ele como um numero, sendo classificado como do tipo int(de integer, interio)
+
+"Antonio" # isso é um texto o python vê ele como uma sequencia de caracteres, sendo classificado como do tipo str(de string, cadeia de caracteres)
+
+True # é um valor literal booleano, classificado como do tipo bool( de boolean, booleano)
+
+
+# existem varios tipos de dados que o python usa para classificar valores literais como:
+
+int # numeros inteiros(sem decimais) ex: 10
+
+float # numeros de ponto flutuante(com decimais) ex: 1.15
+
+str # sequencia de caracteres(texto) ex: "eduardo"
+
+bool # valor logico(verdadeiro ou falso) ex: True
+
+list # uma coleção ordenada mutavel de itens ex: ['banana','laranja','maçã']
+
+tuple # coleção ordenada imutavel de itens ex: ("a", 1)
+
+dict # coleção de pares chave-valor(dicionario) ex: {"chave": "valor"}
+
+set  # coleção não ordenada de itens unicos ex: {"a", "b", "c"}
+
+None # representa a ausencia de volor 
+
+
+# esses são os tipos de dados nativos da linguagem, o interpretador python conhece e entende eles por padrão.
+
+# observação list, tuple, dict, e set também são chamados de estruturas de dados ou tipos de coleção, além de serem tipos de dados.
+
+
+
+
+
+
+
+
+
+
+
